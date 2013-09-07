45626d22caec5f8e513be512596bc49016f2d8e2
Otto Urpelainen <oturpe@iki.fi>
2013-09-07 23:53:39 +0200

    Add Cordova Barcode Scanner plugin + kind of use it

--- www/js/index.js
+++ www/js/index.js
@@ -27,6 +27,9 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+
+        var scanButton = document.getElementById('scanbutton');
+        scanButton.addEventListener('click',this.scan,false);
     },
     // deviceready Event Handler
     //
@@ -45,5 +48,39 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    // Scan barcode
+    //
+    // Calls barcode scanner plugin code.
+    scan: function() {
+        console.log('scanning');
+        try {
+            var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+            scanner.scan( function (result) { 
+
+                alert("We got a barcode\n" + 
+                "Result: " + result.text + "\n" + 
+                "Format: " + result.format + "\n" + 
+                "Cancelled: " + result.cancelled);  
+
+               console.log("Scanner result: \n" +
+                    "text: " + result.text + "\n" +
+                    "format: " + result.format + "\n" +
+                    "cancelled: " + result.cancelled + "\n");
+                document.getElementById("scanresult").innerHTML = result.text;
+                console.log(result);
+                /*
+                if (args.format == "QR_CODE") {
+                    window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+                }
+                */
+
+            }, function (error) { 
+                console.log("Scanning failed: ", error); 
+            } );
+        } catch (ex) {
+            console.log(ex.message);
+        }
     }
 };
